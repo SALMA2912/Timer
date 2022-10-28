31f2736a7282d07be53d2aef99efbde75b52667a
SALMA2912 <112246815+SALMA2912@users.noreply.github.com>
2022-10-29 00:16:58 +0200

    Add files via upload

---
+++ TIM0_cfg.h
@@ -0,0 +1,9 @@
+#ifndef   TIM0_CFG_H
+#define   TIM0_CFG_H
+
+#define TIM0_PPRESCALLER     TIM0_PRESCALLER_8
+#define TIM0_MODE		     TIM0_MODE_NORMAL
+#define TIM0_WF_MODE         TIM0_WF_OFF
+
+
+#endif
